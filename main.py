5fb5b5d071f6bc6ed468a34d00ad31095e41c266
Olena <155203553+elgatoafk@users.noreply.github.com>
2024-04-01 03:06:50 +0300

    Add function to check will it rain

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from processing import *
+from constants import *
+
+weather_data = get_weather_forecast(PARAMETERS)
+
+print(will_rain(weather_data))
--- processing.py
+++ processing.py
@@ -1,8 +1,18 @@
 import requests
 
 def get_weather_forecast(parameters: tuple) -> dict:
+    # gets data for next 12 hours
     response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters, timeout=20)
     response.raise_for_status()
 
-    data = response.json()["list"]
+    return response.json()["list"]
+
+def will_rain(weather_data: list[dict]) -> bool:
+
+    for one_stamp in weather_data:
+        print(one_stamp["weather"][0]["id"])
+        if one_stamp["weather"][0]["id"] <700:
+            return True
+    else:
+        return False
 
